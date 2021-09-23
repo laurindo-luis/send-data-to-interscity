7fdd92fb99d872d436aa982663b41d56e4a53239
Luis Eduardo <luiseduardocosta417@gmail.com>
2021-09-23 17:59:28 -0300

    refactoring data models and send context data

---
+++ src/main/java/br/ufma/lsdi/inserscity/resource/adaptor/ResourceAdaptorService.java
@@ -0,0 +1,33 @@
+package br.ufma.lsdi.inserscity.resource.adaptor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.ufma.lsdi.inserscity.resource.ResourceDto;
+
+@Service
+public class ResourceAdaptorService {
+
+	public ResourceDto registerNewResource(ResourceDto resource) {
+		String url = "http://cidadesinteligentes.lsdi.ufma.br/adaptor/resources";
+		RestTemplate rest = new RestTemplate();
+		
+		ResponseEntity<ResourceDto> response = rest.postForEntity(url, resource, ResourceDto.class);
+		if(response.getStatusCode().value() != 201) 
+			return null;
+		
+		return response.getBody();
+	}
+	
+	public Boolean saveContextData(String uuid, ResourceDto resource) {
+		String url = String.format("http://cidadesinteligentes.lsdi.ufma.br/adaptor/resources"
+				+ "/%s/data", uuid);
+		RestTemplate rest = new RestTemplate();
+		ResponseEntity<Void> response = rest.postForEntity(url, resource, Void.class);
+		return response.getStatusCode().value() == 201 ? true : false;
+	}
+}
