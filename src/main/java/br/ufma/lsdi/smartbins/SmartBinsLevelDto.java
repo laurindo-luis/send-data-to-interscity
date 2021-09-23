ae2977d6865655f10689b991c2f401ba31a6a8c2
Luis Eduardo <luiseduardocosta417@gmail.com>
2021-09-22 21:18:34 -0300

    objects smartbins

---
+++ src/main/java/br/ufma/lsdi/smartbins/SmartBinsLevelDto.java
@@ -0,0 +1,17 @@
+package br.ufma.lsdi.smartbins;
+
+import java.util.List;
+
+public class SmartBinsLevelDto {
+	
+	private List<Record> records;
+
+	public List<Record> getRecords() {
+		return records;
+	}
+
+	public void setRecords(List<Record> records) {
+		this.records = records;
+	}
+	
+}
