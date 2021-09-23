bc8ba858139383210c145e7b9d8e5b81be9b4f28
Luis Eduardo <luiseduardocosta417@gmail.com>
2021-09-22 21:11:45 -0300

    first commit

---
+++ src/main/java/br/com/luis/SendDataToInterscityApplication.java
@@ -0,0 +1,13 @@
+package br.com.luis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendDataToInterscityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendDataToInterscityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luis/SendDataToInterscityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendDataToInterscityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
